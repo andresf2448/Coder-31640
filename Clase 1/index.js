be6a1d5d5b65d864d572d683b4f7dac2b59cff81
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-03-12 16:12:43 -0500

    Clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,77 @@
+/* var nombrePersona;
+nombrePersona = "Andres";
+
+let nombre = 'Camila';
+
+const VALOR = 5;
+
+console.log(VALOR);
+
+let suma = 0;
+
+suma = suma + 1;
+
+let valor = "5";
+
+
+console.log("5" + 3);
+
+let edad = 4;
+
+edad = 5; */
+
+/* let num1 = 4;
+let num2 = 6;
+
+let result = num1 + num2;
+
+console.log(num1 + num2);
+console.log(result);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+let nombre1 = "Franco";
+let nombre2 = "Agostina";
+let saludo = "saluda a";
+
+console.log(nombre1 + "${saludo} " + nombre2);
+console.log(`${nombre1} ${saludo} ${nombre2}`); */
+
+/* console.log(nombre1 + " " + nombre2);
+ */
+
+/* let numero = prompt("Por favor ingrese su nombre");
+let apellidoIngresado = prompt("Por favor ingrese su apellido");
+console.log(typeof(nombreIngresado)); */
+
+/* console.log(`Hola ${nombreIngresado} ${apellidoIngresado}, bienvenid@`); */
+/* alert(`Hola ${nombreIngresado} ${apellidoIngresado}`); */
+
+/* let numero = prompt("ingrese un numero:");
+console.log(`la variable numero es de tipo ${typeof(numero)}`);
+console.log(`La salida convertida es ${typeof(parseInt(numero))}`); */
+
+/* let numero1 = parseInt(prompt("ingrese un numero:"));
+let numero2 = parseInt(prompt("ingrese un numero:"));
+alert(`la suma de los numeros ingresados es: ${numero1 + numero2}`); */
+let nombre = "alejandra";
+let numero = 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(numero);
+
+console.log(3 + numero);
+
